eff7cd4738f144010e13fce890a2e3bcc8ce6c36
prachi1575 <prachi1575.becse24@chitkara.edu.in>
2025-02-05 00:12:56 +0530

    Solution to Check Character Type

--- Check Character Type@@6788dd63490637cfc44e1b95/code.c
+++ Check Character Type@@6788dd63490637cfc44e1b95/code.c
@@ -5,5 +5,8 @@ int main(){
     if(a=='a'|| a=='e'||a=='i'||a=='0'||a=='u'||a=='A'||a=='E'||a=='I'||a=='O'||a=='U'){
         printf("Vowel");
     }
+    else if(a>=A && a<=Z || a>=a && a<=z){
+        printf("Consonant");
+    }
 
 }
